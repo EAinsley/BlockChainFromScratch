5d5063b92082a9a2693942ddf73a286cd2f1b958
Ainsley Su <vhtmscyo@gmail.com>
2026-01-13 17:13:27 +0100

    [Stage 1] Add Hashcontext for hashing complex structural.
    This will be needed by block.

--- src/core/hash.cpp
+++ src/core/hash.cpp
@@ -2,9 +2,13 @@ module;
 
 #include <cstddef>
 #include <iomanip>
-#include <openssl/sha.h>
 #include <ostream>
 #include <span>
+#include <stdexcept>
+#include <utility>
+
+#include <openssl/evp.h>
+#include <openssl/sha.h>
 
 module core.hash;
 
@@ -15,6 +19,55 @@ Sha256Digest sha256(std::span<const std::byte> data) {
          reinterpret_cast<unsigned char *>(digest.data()));
   return digest;
 }
+
+class hash_error : public std::runtime_error {
+public:
+  explicit hash_error(std::string msg) : std::runtime_error(std::move(msg)) {}
+};
+
+Sha256Context::Sha256Context() {
+  ctx_ = EVP_MD_CTX_new();
+  if (!ctx_)
+    throw hash_error{"EVP_MD_CTX_new failed"};
+  if (EVP_DigestInit_ex(ctx_, EVP_sha256(), nullptr) != 1) {
+    EVP_MD_CTX_free(ctx_);
+    throw hash_error{"EVP_DigestInit_ex failed"};
+  }
+}
+Sha256Context::~Sha256Context() {
+  if (ctx_)
+    EVP_MD_CTX_free(ctx_);
+}
+Sha256Context::Sha256Context(Sha256Context &&other) noexcept
+    : ctx_(std::exchange(other.ctx_, nullptr)) {}
+
+Sha256Context &Sha256Context::operator=(Sha256Context &&other) noexcept {
+  if (this != &other) {
+    EVP_MD_CTX_free(ctx_);
+    ctx_ = other.ctx_;
+    other.ctx_ = nullptr;
+  }
+  return *this;
+}
+void Sha256Context::update(std::span<const std::byte> data) {
+  if (!ctx_)
+    throw hash_error{"Uninitialized context"};
+  if (EVP_DigestUpdate(ctx_, data.data(), data.size()) != 1)
+    throw hash_error{"EVP_DigestUpdate failed"};
+}
+Sha256Digest Sha256Context::final() {
+  if (!ctx_)
+    throw hash_error{"Uninitialized context"};
+  Sha256Digest out{};
+  unsigned int outlen = 0;
+  if (EVP_DigestFinal_ex(ctx_, reinterpret_cast<unsigned char *>(out.data()),
+                         &outlen) != 1)
+    throw hash_error{"EVP_DigestFinal_ex failed"};
+  // WARN: We should not allow update after finalize. So We should add a new
+  // member to tract the state and beable to reset.
+  return out;
+}
+
 } // namespace core
 
 std::ostream &operator<<(std::ostream &os, const core::Sha256Digest &digest) {
