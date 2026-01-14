42ed32dccc4c5ab21f2d72730bef7d18ea42a1a1
Ainsley Su <vhtmscyo@gmail.com>
2026-01-14 13:28:47 +0100

    [Stage 1] Refine Hashcontext.

--- src/core/hash.cpp
+++ src/core/hash.cpp
@@ -25,49 +25,71 @@ public:
   explicit hash_error(std::string msg) : std::runtime_error(std::move(msg)) {}
 };
 
-Sha256Context::Sha256Context() {
-  ctx_ = EVP_MD_CTX_new();
+Sha256Context::Sha256Context() { reset(); }
+
+Sha256Context::~Sha256Context() {
+  if (ctx_)
+    EVP_MD_CTX_free(ctx_);
+}
+
+Sha256Context::Sha256Context(Sha256Context &&other) noexcept
+    : ctx_(std::exchange(other.ctx_, nullptr)),
+      last_digest_(std::move(other.last_digest_)),
+      state_(std::exchange(other.state_, State::Uninitialized)) {}
+
+Sha256Context &Sha256Context::operator=(Sha256Context &&other) noexcept {
+  if (this != &other) {
+    std::swap(ctx_, other.ctx_);
+    last_digest_ = std::move(other.last_digest_);
+    std::swap(state_, other.state_);
+    other.invalidate();
+  }
+  return *this;
+}
+
+void Sha256Context::update(std::span<const std::byte> data) {
+  if (state_ == State::Uninitialized)
+    throw hash_error{"Uninitialized context. Reset first."};
+  if (state_ == State::Finalized)
+    throw hash_error("Context is finalized. Reset first.");
+  if (EVP_DigestUpdate(ctx_, data.data(), data.size()) != 1)
+    throw hash_error{"EVP_DigestUpdate failed"};
+}
+
+Sha256Digest Sha256Context::final() {
+  if (state_ == State::Uninitialized)
+    throw hash_error{"Uninitialized context. Reset first"};
+  unsigned int outlen = 0;
+  if (state_ == State::Finalized)
+    return last_digest_;
+
+  if (EVP_DigestFinal_ex(ctx_,
+                         reinterpret_cast<unsigned char *>(last_digest_.data()),
+                         &outlen) != 1)
+    throw hash_error{"EVP_DigestFinal_ex failed"};
+  state_ = State::Finalized;
+  return last_digest_;
+}
+
+bool Sha256Context::finalized() const { return state_ == State::Finalized; }
+void Sha256Context::invalidate() {
+  if (ctx_)
+    EVP_MD_CTX_free(ctx_);
+  ctx_ = nullptr;
+  state_ = State::Uninitialized;
+}
+bool Sha256Context::valid() const { return state_ != State::Uninitialized; }
+void Sha256Context::reset() {
+  if (!ctx_)
+    ctx_ = EVP_MD_CTX_new();
   if (!ctx_)
     throw hash_error{"EVP_MD_CTX_new failed"};
   if (EVP_DigestInit_ex(ctx_, EVP_sha256(), nullptr) != 1) {
     EVP_MD_CTX_free(ctx_);
     throw hash_error{"EVP_DigestInit_ex failed"};
   }
+  state_ = State::Updating;
 }
-Sha256Context::~Sha256Context() {
-  if (ctx_)
-    EVP_MD_CTX_free(ctx_);
-}
-Sha256Context::Sha256Context(Sha256Context &&other) noexcept
-    : ctx_(std::exchange(other.ctx_, nullptr)) {}
-
-Sha256Context &Sha256Context::operator=(Sha256Context &&other) noexcept {
-  if (this != &other) {
-    EVP_MD_CTX_free(ctx_);
-    ctx_ = other.ctx_;
-    other.ctx_ = nullptr;
-  }
-  return *this;
-}
-void Sha256Context::update(std::span<const std::byte> data) {
-  if (!ctx_)
-    throw hash_error{"Uninitialized context"};
-  if (EVP_DigestUpdate(ctx_, data.data(), data.size()) != 1)
-    throw hash_error{"EVP_DigestUpdate failed"};
-}
-Sha256Digest Sha256Context::final() {
-  if (!ctx_)
-    throw hash_error{"Uninitialized context"};
-  Sha256Digest out{};
-  unsigned int outlen = 0;
-  if (EVP_DigestFinal_ex(ctx_, reinterpret_cast<unsigned char *>(out.data()),
-                         &outlen) != 1)
-    throw hash_error{"EVP_DigestFinal_ex failed"};
-  // WARN: We should not allow update after finalize. So We should add a new
-  // member to tract the state and beable to reset.
-  return out;
-}
-
 } // namespace core
 
 std::ostream &operator<<(std::ostream &os, const core::Sha256Digest &digest) {
